3e5cab54972adb42df4ddfed84f51fc6f033fd38
Boyd Ndonga <boydbugle@gmail.com>
2018-07-31 22:21:49 +0300

    cofigure linting file setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  goals: string[];
+
+  constructor(){
+
+    this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
+  }
 }
