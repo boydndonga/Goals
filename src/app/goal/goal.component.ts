ff00423679d6658f863e07739a2733d76ab03428
Boyd Ndonga <boydbugle@gmail.com>
2018-08-01 01:25:24 +0300

    restructure Goal class to include completeDate

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalComponent implements OnInit {
   goals = [
-    new Goal(1, 'Watch Finding Nemo', 'first desc'),
-    new Goal(2, 'Buy Cookies', 'second desc'),
-    new Goal(3, 'Get new Phone Case', 'third desc'),
-    new Goal(4, 'Get Dog Food', 'fourth desc'),
-    new Goal(5, 'Solve math homework', 'fifth desc'),
-    new Goal(6, 'Plot my world domination plan', 'final desc'), ];
+    new Goal(1, 'Watch Finding Nemo', 'first desc', new Date(2018, 6, 9)),
+    new Goal(2, 'Buy Cookies', 'second desc', new Date(2018, 10, 12)),
+    new Goal(3, 'Get new Phone Case', 'third desc', new Date(2018, 8, 15)),
+    new Goal(4, 'Get Dog Food', 'fourth desc', new Date(2018, 4, 10)),
+    new Goal(5, 'Solve math homework', 'fifth desc', new Date(2018, 6, 19)),
+    new Goal(6, 'Plot my world domination plan', 'final desc', new Date(2018, 3, 20)), ];
 
     toogleDetails(index) {
       this.goals[index].showDescription = !this.goals[index].showDescription;
