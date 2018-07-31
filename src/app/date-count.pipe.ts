c90f60bc9bec4c6226326f93b72b3bfe83c99753
Boyd Ndonga <boydbugle@gmail.com>
2018-08-01 02:12:26 +0300

    pipe output through custom dateCount pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any): any {
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const dateDifference = Math.abs(value - todayWithNoTime ); // returns value in millisecond
+    const secondsInADay = 86400; // 60 *60 *24hrs
+
+    const dateDifferenceSeconds = dateDifference * 0.001; // converts to seconds
+
+    const dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if (dateCounter >= 1) {
+        return dateCounter;
+    } else {
+        return 0;
+    }
   }
 
 }
